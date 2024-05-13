9eed246da446c060d207ae50fb8dbdad72ec08b3
Emanuel Castro <emanuelsmcastro3@gmail.com>
2024-05-12 21:05:57 -0300

    initial commit

---
+++ literalura/src/test/java/com/alura/challenge/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
